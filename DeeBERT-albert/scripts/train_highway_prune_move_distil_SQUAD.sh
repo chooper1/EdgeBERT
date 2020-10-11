94e635c8a394058d8f638a854c265c413f3ec488
chooper1 <54963573+chooper1@users.noreply.github.com>
2020-10-10 21:49:40 -0300

    Cleaning Up

--- DeeBERT-albert/scripts/train_highway_prune_move_distil_SQUAD.sh
+++ DeeBERT-albert/scripts/train_highway_prune_move_distil_SQUAD.sh
@@ -9,7 +9,7 @@ MODEL_NAME=${MOD_NAME}-${MODEL_SIZE}
 EPOCHS=10
 if [ $MODEL_TYPE = 'bert' ]
 then
-  EPOCHS=3
+  EPOCHS=10
   MODEL_NAME=${MODEL_NAME}-uncased
 fi
 if [ $MOD_NAME = 'albert' ]
@@ -21,7 +21,7 @@ fi
 SQUAD_DATA=./squad/
 DATASET=squad
 
-THRESHOLDS="1 0.8 0.6 0.4 0.2 0.1"
+THRESHOLDS="1 0.8 0.6"
 
 for FINAL_THRESH in $THRESHOLDS; do
   echo $FINAL_THRESH
@@ -41,7 +41,7 @@ for FINAL_THRESH in $THRESHOLDS; do
     --num_train_epochs $EPOCHS \
     --overwrite_output_dir \
     --seed 42 \
-    --output_dir ./saved_models/${MODEL_TYPE}-${MODEL_SIZE}/$DATASET/two_stage_pruned_${FINAL_THRESH} \
+    --output_dir ./saved_models/${MODEL_TYPE}-${MODEL_SIZE}/$DATASET/two_stage_pruned_${FINAL_THRESH}_distil \
     --save_steps 0 \
     --overwrite_cache \
     --warmup_steps 5400 \
@@ -50,6 +50,6 @@ for FINAL_THRESH in $THRESHOLDS; do
     --initial_warmup 1 --final_warmup 2 \
     --pruning_method topK --mask_init constant --mask_scale 0. \
     --version_2_with_negative \
-    --teacher_type albert_teacher --teacher_name_or_path /n/holyscratch01/acc_lab/chooper/albert-py/DeeBERT-albert/scripts/saved_models/albert-base/$DATASET/teacher/ \
+    --teacher_type albert_teacher --teacher_name_or_path /n/holyscratch01/acc_lab/chooper/albert-py/DeeBERT-albert/scripts/saved_models/albert-base/$DATASET/teacher \
     --alpha_ce 0.5 --alpha_distil 0.5
 done
