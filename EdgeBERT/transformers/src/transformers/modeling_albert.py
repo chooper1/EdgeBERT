b1015b9dc5d98e01c50f20bcb46ebbc65006f29d
chooper1 <54963573+chooper1@users.noreply.github.com>
2020-10-16 11:25:15 -0300

    edge

--- EdgeBERT/transformers/src/transformers/modeling_albert.py
+++ EdgeBERT/transformers/src/transformers/modeling_albert.py
@@ -316,7 +316,7 @@ class AlbertLayerGroup(nn.Module):
 
 class AlbertTransformer(nn.Module):
     def __init__(self, config, params):
-        super().__init__(config, params)
+        super().__init__()
 
         self.config = config
         self.output_attentions = config.output_attentions
