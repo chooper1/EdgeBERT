cc03c74d2dc559a37abf5ac27af28fd2b6d0faf4
chooper1 <54963573+chooper1@users.noreply.github.com>
2020-10-27 21:13:05 -0300

    Update count_params.sh

--- EdgeBERT/scripts/count_params.sh
+++ EdgeBERT/scripts/count_params.sh
@@ -5,12 +5,13 @@ MODEL_TYPE=masked_albert
 MODEL_SIZE=base
 MODEL_NAME=${MODEL_TYPE}-${MODEL_SIZE}
 DATASET=MNLI
-THRESH="1 10"
+THRESH="0.8 0.6 0.5 0.4 0.2 0.1"
 
 for FINAL_THRESH in ${THRESH}; do
-PATHNAME=./saved_models/${MODEL_TYPE}-${MODEL_SIZE}/${DATASET}/soft_move_pruned_${FINAL_THRESH}
+
+PATHNAME=./saved_models/${MODEL_TYPE}-${MODEL_SIZE}/${DATASET}/move_pruned_${FINAL_THRESH}
 python ../examples/counts_parameters.py \
-    --pruning_method sigmoied_threshold \
-    --threshold 0.1 \
-    --model_name_or_path $PATHNAME \
+    --pruning_method  topK \
+    --threshold $FINAL_THRESH \
+    --serialization_dir $PATHNAME
 done
